6bd6a514429e21740f5042e40a81209ee8f0be47
David Glasser <glasser@meteor.com>
2013-10-22 23:16:52 -0700

    Add some facts to oplog.

--- packages/mongo-livedata/mongo_driver.js
+++ packages/mongo-livedata/mongo_driver.js
@@ -267,10 +267,14 @@ MongoConnection.prototype._startOplogTailing = function (oplogUrl, dbName) {
       if (!_.has(callbacksByCollection, collectionName))
         callbacksByCollection[collectionName] = {};
       var callbackId = nextId++;
+      Package.facts && Package.facts.Facts.incrementServerFact(
+        "mongo-livedata", "oplog-watchers", 1);
       callbacksByCollection[collectionName][callbackId] = callback;
       return {
         stop: function () {
           delete callbacksByCollection[collectionName][callbackId];
+          Package.facts && Package.facts.Facts.incrementServerFact(
+            "mongo-livedata", "oplog-watchers", -1);
         }
       };
     },
